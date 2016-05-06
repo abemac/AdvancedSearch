cabd117464a23ed556ad9037e026b6468e61f95f
Abraham McIlvaine <abemac@udel.edu>
2016-05-06 19:23:03 -0400

    new stuff

--- WindowsBuild/build.bat
+++ WindowsBuild/build.bat
@@ -1,4 +1,4 @@
-g++ -std=c++11 ..\HashedObj.cpp ..\HashTable.cpp ..\main.cpp -o ..\run\win_output
+g++ -std=c++11 ..\project2\HashedObj.cpp ..\project2\HashTable.cpp ..\project1\Node.cpp ..\project1\Graph.cpp ..\main.cpp -o ..\run\win_output
 echo "Press any key to run"
 pause >nul
 ..\run\win_output.exe
