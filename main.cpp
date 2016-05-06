27b30210f42326176f996d7fca369650f51259c4
tsasO <tsas@udel.edu>
2016-05-06 17:43:17 -0400

    new stuff

--- main.cpp
+++ main.cpp
@@ -13,16 +13,7 @@ int main(){
 
   ofstream file;
   file.open("txt41.txt");
-  int length=0;
-  for(unsigned int i=0; i< q.size();i++){
-    file << q[i];//writes each poreccessed word out individually
-    length+=1;
-    file<< " ";//space between words
-    if(length>=80){
-      file<<"\n";//new lines every 80 characters
-      length=0;
-    }
-  }
+  file>>q;
   file.close();
 
 }
