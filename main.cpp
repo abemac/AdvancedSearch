093a5ab8cc35a649c403f360a483f447db5a138f
SuperG <SuperG@wifi-roaming-128-4-230-213.host.udel.edu>
2016-05-06 17:35:12 -0400

    new stuff

--- main.cpp
+++ main.cpp
@@ -1,30 +1,18 @@
-#include <iostream>
-#include <ofstream>
 #include "project2/stem.cpp"
-<<<<<<< HEAD
 #include "project1/graphMain.cpp"
 #include "project1/Graph.h"
-=======
 using namespace std;
 
->>>>>>> 80b128581af302b8bfb668c6d4362bc00e047629
-
-using namespace std;
 int main(){
-<<<<<<< HEAD
+
   stem();
   graphMain();
-  cout<<isValidInput("cat")<<endl;
-  return 0;
-=======
   string q;
   cout<<"Input Query String"<<endl;
-  cin>>q>>endl;
+  cin>>q;
 
   ofstream ofs;
   ofs.open("txt41.txt", out | trunc);
   ofs<<q;
   ofs.close();
-
->>>>>>> 80b128581af302b8bfb668c6d4362bc00e047629
 }
