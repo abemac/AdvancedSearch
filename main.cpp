29162af604cb538ee998d0db8755a9eb724d0e93
SuperG <SuperG@wifi-roaming-128-4-225-44.host.udel.edu>
2016-05-10 15:28:38 -0400

    new stuff

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "project2/stem.cpp"
 #include "project1/graphMain.cpp"
 #include "project1/Graph.h"
+#include <vector>
 using namespace std;
 void readFiles();
 int wordCount();
 vector<string> dict;
-double frequency[];
-int * docNum[];
+//double frequency[];
+//int * docNum[];
 void inputQuery();
 void loadDict();
 bool isThere(string word);
@@ -15,10 +16,10 @@ bool isThere(string word);
 
 int main(){
   graphMain();
-  inputQuery();
+  //inputQuery();
   stem();
-
-
+  loadDict();
+  return 0;
 }
 
 void inputQuery(){
@@ -33,7 +34,54 @@ void inputQuery(){
 }
 
 void loadDict(){
+  for(int i=0; i<40; i++){
+    string path;
+    if(os.compare("Windows")==0){
+  		if(fileNum<10){
+  			path="corpus\\txt0"+to_string(i+1)+"_cleaned.txt";
+  		}
+      else{
+  			path="corpus\\txt"+to_string(i+1)+"_cleaned.txt";
+  		}
+  	}
+    else{
+  		if(fileNum<10){
+  			path="corpus/txt0"+to_string(i+1)+"_cleaned.txt";
+  		}else{
+  			path="corpus/txt"+to_string(i+1)+"_cleaned.txt";
+  		}
+    }
 
+    int x=0;
+  	char c;
+  	fstream textfile;
+  	textfile.open(path);
+    string word="";
+  	while (!textfile.eof()){
+  			c=textfile.get();//assign a char to c
+  			while(c==' '){
+  				c=textfile.get();
+  			}
+  			//word.push_back("");//create a new space for new word
+  			while((c!=',' && c!=' ' && c!='\n') && !textfile.eof()){//split words by ',' ' ' '\n'
+
+  				if(c>=97&& c<=122)//only include letters
+  					word=word+c;//build words char by char
+  				c=textfile.get();
+  			}
+        if(!isThere(word)){
+          dict.push_back(word);
+        }
+  			if(dict.size()==0)
+  				dict.pop_back();
+  			else{
+  				x++;
+  			}
+  	}
+
+  	textfile.close();
+
+  }
 }
 bool isThere(string word){
   for(int i=0; i<dict.size(); i++){
